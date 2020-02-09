698865b8257400df5b3439db71ef52f3024b1adf
Andrii Shtadler <andrii_shtadler@epam.com>
2020-02-09 13:54:11 +0200

    asd

--- actions/set-hello/index.js
+++ actions/set-hello/index.js
@@ -1,7 +1,14 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
 
 try {
-  core.setOutput("cool_message", 'HIII MYYYYCOOOLLL 1231242341!!!');
+  // `who-to-greet` input defined in action metadata file
+  console.log(`Hello set`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
